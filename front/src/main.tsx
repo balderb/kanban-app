b18947afc25907432592d95ded0f9bad8daa8795
Balder Bongaerts <59473814+balderb@users.noreply.github.com>
2023-02-20 21:28:54 +0100

    feat: add chakra ui package and config on root

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import * as React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
 import store from './store/store'
 import './index.css'
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>
+  <ChakraProvider>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  </ChakraProvider>
 )
