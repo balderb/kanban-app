f8a2e01f3caa887d9c7cae2b27518d613d15aa65
Balder Bongaerts <59473814+balderb@users.noreply.github.com>
2023-02-20 22:52:55 +0100

    chore: init change for app component

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,10 @@
-import React from "react";
+import React from 'react';
 import './index.css'
 
 function App() {
-
   return (
     <div>
-      <h1 className="underline decoration-sky-500">Hello world</h1>
+      Kanban app
     </div>
   )
 }
